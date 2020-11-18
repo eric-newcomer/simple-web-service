afacc8c3d897278fbcfa3cacd0ff29e376159df4
Eric Newcomer <enewcomer23@gmail.com>
2020-11-18 14:22:30 -0800

    Verify that I can see lat and long

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const formidable = require('express-formidable');
 const fs = require('fs');
+const ExifImage = require('exif').ExifImage;
+const exif = require('exif-parser')
 
 const app = express();
 const port = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(formidable());
+
+// getZipcode: String filename -> String Zipcode
+const getZipcode = (filename) => {
+   try {
+      new ExifImage({ image : filename }, (error, exifData) => {
+          if (error)
+              console.log('Error: '+error.message);
+          else
+              console.log(exifData); // Do something with your data!
+      });
+  } catch (error) {
+      console.log('Error: ' + error.message);
+  }
+}
 
 app.post("/post", (req, res) => {
    console.log('in server')
@@ -31,10 +47,12 @@ app.post("/post", (req, res) => {
    const mimeType = fileAsBase64.substring(fileAsBase64.indexOf(":")+1, fileAsBase64.indexOf(";"));
    let isJPG = false;
    let msg = "File is not a JPEG.";
+   let zipCode = '';
    if (mimeType === "image/jpeg") { 
       isJPG = true;
       msg = "File is a JPEG!";
       console.log("Is a JPEG!");
+      zipcode = getZipcode(filename);
    } else {
       console.log("Not a JPEG");
    }
